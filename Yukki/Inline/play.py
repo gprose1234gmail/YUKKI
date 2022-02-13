50f65b35a1c22492b7de891a1d0697d65351f266
gprose1234gmail <95291555+gprose1234gmail@users.noreply.github.com>
2022-02-13 10:27:36 +0530

    Update play.py

--- Yukki/Inline/play.py
+++ Yukki/Inline/play.py
@@ -210,6 +210,8 @@ def primary_markup(videoid, user_id, current_time, total_time):
         [
             InlineKeyboardButton(text="▶️", callback_data=f"resumecb"),
             InlineKeyboardButton(text="⏸️", callback_data=f"pausecb"),
+          InlineKeyboardButton(
+                text=f"{total_time}{current_time}",callback_data=f"timer_checkup_markup {videoid}|{user_id}"),
             InlineKeyboardButton(text="⏭️", callback_data=f"skipcb"),
             InlineKeyboardButton(text="⏹️", callback_data=f"stopcb"),
         ],
@@ -234,6 +236,8 @@ def timer_markup(videoid, user_id, current_time, total_time):
         [
             InlineKeyboardButton(text="▶️", callback_data=f"resumecb"),
             InlineKeyboardButton(text="⏸️", callback_data=f"pausecb"),
+          InlineKeyboardButton(
+                text=f"{total_time}{current_time}",callback_data=f"timer_checkup_markup {videoid}|{user_id}"),
             InlineKeyboardButton(text="⏭️", callback_data=f"skipcb"),
             InlineKeyboardButton(text="⏹️", callback_data=f"stopcb"),
         ],
@@ -261,6 +265,8 @@ def audio_markup(videoid, user_id, current_time, total_time):
         [
             InlineKeyboardButton(text="▶️", callback_data=f"resumecb"),
             InlineKeyboardButton(text="⏸️", callback_data=f"pausecb"),
+          InlineKeyboardButton(
+                text=f"{total_time}{current_time}",callback_data=f"timer_checkup_markup {videoid}|{user_id}"),
             InlineKeyboardButton(text="⏭️", callback_data=f"skipcb"),
             InlineKeyboardButton(text="⏹️", callback_data=f"stopcb"),
         ],
@@ -280,6 +286,8 @@ def audio_timer_markup_start(videoid, user_id, current_time, total_time):
         [
             InlineKeyboardButton(text="▶️", callback_data=f"resumecb"),
             InlineKeyboardButton(text="⏸️", callback_data=f"pausecb"),
+          InlineKeyboardButton(
+                text=f"{total_time}{current_time}",callback_data=f"timer_checkup_markup {videoid}|{user_id}"),
             InlineKeyboardButton(text="⏭️", callback_data=f"skipcb"),
             InlineKeyboardButton(text="⏹️", callback_data=f"stopcb"),
         ],
